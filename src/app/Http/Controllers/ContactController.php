97ad2ced9d406c5d21057f11e9895040a4e25f7a
tangrowth <to.chinatsu@icloud.com>
2023-04-13 14:01:23 +0900

    0-4 フォーム入力ページのボタン

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Contact;
+use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
   public function index()
   {
     return view('index');
+  }
+
+  public function confirm(Request $request)
+  {
+    $contact = $request->only(['name', 'email', 'tel', 'content']);
+    return view('confirm', compact('contact'));
   }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 
 Route::get('/', [ContactController::class, 'index']);
+Route::post('/contacts/confirm', [ContactController::class, 'confirm']);
